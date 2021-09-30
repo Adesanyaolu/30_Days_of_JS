4ef9cc81606348be97528f1bcfe9893c00bb3fa4
AdeSanyaOlu <Adesanyaolu@users.noreply.github.com>
2021-09-29 20:53:15 -0700

    Task 5 updated

---
+++ 05_Day_Arrays/05_day_starter/scripts/countries.js
@@ -0,0 +1,13 @@
+const country = [
+    'Albania',
+    'Bolivia',
+    'Canada',
+    'Denmark',
+    'Ethiopia',
+    'Finland',
+    'Germany',
+    'Hungary',
+    'Ireland',
+    'Japan',
+    'Kenya'
+  ];
