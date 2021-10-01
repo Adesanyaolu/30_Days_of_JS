dc93f09f717b2b88950644957f97256e889dcb92
AdeSanyaOlu <Adesanyaolu@users.noreply.github.com>
2021-10-01 00:46:19 -0700

    Started day 6 exercises

--- 06_Day_Loops/06_day_starter/scripts/main.js
+++ 06_Day_Loops/06_day_starter/scripts/main.js
@@ -1,2 +1,15 @@
 console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+// alert('Open the console and check if the countries has been loaded')
+for(let i = 10; i >= 0; i--){
+    console.log(i)
+  }
+let f = 10
+while (f >= 0){
+    console.log (f)
+    f--
+}
+ let g = 10
+ do {
+     console.log (g)
+     g--
+ } while (g >= 0)
