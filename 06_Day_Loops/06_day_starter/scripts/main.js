00de83efc9ab4c61c87c725701dad11e42bb03da
AdeSanyaOlu <sanya4reel@gmail.com>
2022-03-05 22:37:09 +0100

    Part of Day 7 exercise

--- 06_Day_Loops/06_day_starter/scripts/main.js
+++ 06_Day_Loops/06_day_starter/scripts/main.js
@@ -1,4 +1,4 @@
-console.log(countries)
+/**  console.log(countries)
 // alert('Open the console and check if the countries has been loaded')
 for(let i = 10; i >= 0; i--){
     console.log(i)
@@ -14,3 +14,99 @@ while (f >= 0){
      g--
  } while (g >= 0)
  console.table(2)
+
+const t = ["#", "##", "###", "####", "#####", "######", "#######"];
+let tLength = t.length;
+for (let i = 0; i < t.length; i++){
+    console.log(t[i])
+}
+
+const dig = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for (let i = 0; i < dig.length; i++){
+    console.log(`${dig[i]} x ${dig[i]} = ${dig[i] * dig[i]} `)
+}
+
+const nDig = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for (let i = 0; i < nDig.length; i++){
+    console.log(`${nDig[i]} ${(nDig[i])**2} ${(nDig[i])**3}`)
+}
+
+for (let i = 0; i <= 100; i++){
+    if (i % 2 == 0){
+        console.log(i)
+    }
+}
+
+for (let i = 0; i <= 100; i++){
+    if (i % 2 !== 0){
+        console.log(i)
+    }
+}
+
+var newArr = []
+for (let i = 0; i <= 100; i++){ 
+    let newArr = []
+    newArr.push(i) 
+    console.log (newArr)
+    let sum = 0
+    for (const num of newArr) {
+    sum += sum + num 
+    console.log(sum)
+}
+}
+
+var arr = []
+while(arr.length < 5){
+    var r = Math.floor(Math.random()*100) + 1;
+    if(arr.indexOf(r) === -1) arr.push(r);
+}
+console.log(arr)
+
+
+
+
+**/
+// const numbers = 100;
+// let sum = 0
+// for(let i = 0; i < numbers.length; i++){
+//   sum  = sum + numbers[i]  // can be shorten, sum += numbers[i]
+
+// }
+
+// console.log(sum)  // 15
+
+// const newArr = []
+// let sum = 0
+// for (let i = 0; i <= 100; i++){
+//    newArr.push(i)
+//    sum  += newArr[i]
+// }
+// console.log(newArr)
+// console.log(sum);
+
+const evenOddSum = []
+let sum  = 0
+for (let i = 0; i <= 100; i++){
+    if (i % 2 == 0){
+       evenOddSum.push(sum = sum + i)
+    }
+}
+console.log(sum)
+console.log(evenOddSum[50])
+
+for (let i = 0; i <= 100; i++){
+    if (i % 2 !== 0){
+        evenOddSum.push(sum = sum + i)
+    }
+}
+console.log(sum)
+console.log(evenOddSum[50]);
+
+
+let testArray = []
+for (i = 1; i <= 5; i++ ){
+    let test = (Math.floor(Math.random() * 5))
+    testArray.push(test)
+    console.log(testArray)
+}
+
--- 07_Day_Functions/07_day_starter/scripts/main.js
+++ 07_Day_Functions/07_day_starter/scripts/main.js
@@ -1,2 +1,87 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+// console.log(countries)
+// alert('Open the console and check if the countries has been loaded')
+function fullName (){
+    let firstName = "Adesanya"
+    let lastName = "Adenuga"
+    console.log(firstName + " " + lastName)
+}
+fullName()
+function addNumbers(par1, par2){
+    let sum = par1 + par2
+    console.log(sum)
+}
+addNumbers(2,6)
+
+function areaOfRectangle(length, width){
+    let area = length * width
+    return area
+}
+console.log(areaOfRectangle(2,3));
+
+function volumeOfRectPrism(lenght, width, height){
+    let rectPrism = lenght * width * height
+    return rectPrism
+}
+console.log(volumeOfRectPrism(3,4,5));
+// 12 
+function convertCelciusToFahrenheit (oC){
+    let oF = (oC * 9/5) + 32
+    return oF
+}
+console.log("No 12:" + convertCelciusToFahrenheit(100));
+
+//13 
+function calcBMI (weight, height){
+    let bmi = weight / (height * height);
+    
+    if (bmi < 18.5) {
+        return (`${bmi} indicates underweight`)
+    } else if (18.5 > bmi || bmi < 24.9) {
+        return (`${bmi} indicates normal weight`)
+    }   else if (25 > bmi || bmi < 29.9) {
+        return (`${bmi} indicates overweight`)
+    } else if (bmi >= 30){
+        return (`${bmi} indicates Obese`)
+    }
+}
+console.log(calcBMI(1500,20));
+
+//14 
+const months = ["January", 
+                "February", 
+                "March", 
+                "April", 
+                "May", 
+                "June", 
+                "July", 
+                "August", 
+                "September", 
+                "October", 
+                "November", 
+                "December"];
+/* function checkSeason (month){
+    if (months[0]) {
+        return "Jan"
+    }
+}
+console.log(checkSeason(0)) */
+
+//15
+function findMax (dig1, dig2, dig3){
+
+   return Math.max(dig1,dig2,dig3)
+}
+console.log(findMax(500,10000,20));
+
+// Level 2 question 2
+function solveQuadEquation (a,b,c,){
+    let expo = (Math.sqrt(b**2 - (4*a*c)))/2*a
+    let expoAns =  parseInt(expo)
+    console.log (expoAns)
+    
+
+}
+console.log(solveQuadEquation(2,3,4))
+
+
+
